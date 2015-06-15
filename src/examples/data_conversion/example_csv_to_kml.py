5eceafe6b33f78f333ac0ac46285251c5e517ada
recombinant <pykml@recombinant.me.uk>
2015-06-15 12:22:18 +0100

    example_csv_to_kml.py updated to use the latest csv feed from http://earthquake.usgs.gov/earthquakes/feed

--- src/examples/data_conversion/example_csv_to_kml.py
+++ src/examples/data_conversion/example_csv_to_kml.py
@@ -4,92 +4,115 @@
 
 References:
 
+http://earthquake.usgs.gov/earthquakes/feed/
+http://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
 """
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+# from __future__ import unicode_literals
 import csv
-import urllib2
 from datetime import datetime
+
 from lxml import etree
+
+from six.moves.urllib.request import urlopen
 from pykml.factory import KML_ElementMaker as KML
+from pykml.parser import Schema
+from pykml.util import format_xml_with_cdata
 
 
-def makeExtendedDataElements(datadict):
+def make_extended_data_elements(datadict):
     """Converts a dictionary to ExtendedData/Data elements"""
     edata = KML.ExtendedData()
-    for key, value in datadict.iteritems():
-        edata.append(KML.Data(KML.value(value), name=key + "_"))
+    for key, value in datadict.items():
+        edata.append(KML.Data(KML.value(value), name=key + '_'))
     return edata
 
-# create a KML document with a folder and a default style
-stylename = "earthquake-balloon-style"
-balloonstyle = KML.BalloonStyle(
-    KML.text("""
-<table Border=1>
-  <tr><th>Earthquake ID</th><td>$[Eqid_]</td></tr>
-  <tr><th>Magnitude</th><td>$[Magnitude_]</td></tr>
-  <tr><th>Depth</th><td>$[Depth_]</td></tr>
-  <tr><th>Datetime</th><td>$[Datetime_]</td></tr>
-  <tr><th>Coordinates</th><td>($[Lat_],$[Lat_])</td></tr>
-  <tr><th>Region</th><td>$[Region_]</td></tr>
-</table>"""
-             ),
-)
 
-doc = KML.Document()
+def get_balloon_style():
+    """
+    A separate instance of KML.BalloonStyle() needs to be created for each
+    KML.Style(). The KML.text() will be formatted as CDATA
+    """
+    return KML.BalloonStyle(
+        KML.text("""
+        <table Border=1>
+          <tr><th>Earthquake ID</th><td>$[id_]</td></tr>
+          <tr><th>Magnitude</th><td>$[mag_]</td></tr>
+          <tr><th>Depth</th><td>$[depth_]</td></tr>
+          <tr><th>Datetime</th><td>$[time_]</td></tr>
+          <tr><th>Coordinates</th><td>($[longitude_],$[latitude_])</td></tr>
+          <tr><th>Region</th><td>$[place_]</td></tr>
+        </table>"""),
+    )
 
-iconstyles = [
-    [2, 'ff000000'],
-    [3, 'ffff0000'],
-    [4, 'ff00ff55'],
-    [5, 'ffff00aa'],
-    [6, 'ff00ffff'],
-    [7, 'ff0000ff'],
-]
 
-# create a series of Icon Styles
-for threshold, color in iconstyles:
-    doc.append(
-        KML.Style(
-            KML.IconStyle(
-                KML.color(color),
-                KML.scale(threshold / 2),
-                KML.Icon(
-                    KML.href("http://maps.google.com/mapfiles/kml/shapes/earthquake.png"),
+def main():
+    """
+    Create a KML document with a folder and a style for each earthquake
+    magnitude
+    """
+    doc = KML.Document()
+
+    icon_styles = [
+        [2, 'ff000000'],
+        [3, 'ffff0000'],
+        [4, 'ff00ff55'],
+        [5, 'ffff00aa'],
+        [6, 'ff00ffff'],
+        [7, 'ff0000ff'],
+    ]
+
+    # create a series of Icon Styles
+    for threshold, color in icon_styles:
+        doc.append(
+            KML.Style(
+                KML.IconStyle(
+                    KML.color(color),
+                    KML.scale(threshold / 2),
+                    KML.Icon(
+                        KML.href('http://maps.google.com/mapfiles/kml/shapes/earthquake.png'),
+                    ),
+                    KML.hotSpot(x='0.5', y='0', xunits='fraction', yunits='fraction'),
                 ),
-                KML.hotSpot(x="0.5", y="0", xunits="fraction", yunits="fraction"),
-            ),
-            balloonstyle,
-            id="earthquake-style-{threshold}".format(threshold=threshold),
-        )
-    )
-
-doc.append(KML.Folder())
-
-# read in a csv file, and create a placemark for each record
-url = "http://earthquake.usgs.gov/earthquakes/catalogs/eqs7day-M2.5.txt"
-fileobject = urllib2.urlopen(url)
-for row in csv.DictReader(fileobject):
-    timestamp = datetime.strptime(row["Datetime"], "%A, %B %d, %Y %H:%M:%S %Z")
-    pm = KML.Placemark(
-        KML.name("Magnitude={0}".format(row['Magnitude'])),
-        KML.TimeStamp(
-            KML.when(timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')),
-        ),
-        KML.styleUrl(
-            "#earthquake-style-{thresh}".format(
-                thresh=int(float(row['Magnitude']))
+                get_balloon_style(),
+                id='earthquake-style-{threshold}'.format(threshold=threshold),
             )
-        ),
-        makeExtendedDataElements(row),
-        KML.Point(
-            KML.coordinates("{0},{1}".format(row["Lon"], row["Lat"]))
         )
-    )
-    doc.Folder.append(pm)
 
-# check if the schema is valid
-from pykml.parser import Schema
+    doc.append(KML.Folder())
 
-schema_gx = Schema("kml22gx.xsd")
-schema_gx.assertValid(doc)
+    # read in a csv file, and create a placemark for each record
+    url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.csv'
+    fileobject = urlopen(url)
+    for row in csv.DictReader(fileobject):
+        timestamp = datetime.strptime(row['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        pm = KML.Placemark(
+            KML.name('Magnitude={0}'.format(row['mag'])),
+            KML.TimeStamp(
+                KML.when(timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')),
+            ),
+            KML.styleUrl(
+                '#earthquake-style-{thresh}'.format(
+                    thresh=int(float(row['mag']))
+                )
+            ),
+            make_extended_data_elements(row),
+            KML.Point(
+                KML.coordinates('{0},{1}'.format(row['longitude'], row['latitude']))
+            )
+        )
+        doc.Folder.append(pm)
 
-print(etree.tostring(doc, pretty_print=True))
+    # check if the schema is valid
+    schema_gx = Schema('kml22gx.xsd')
+    schema_gx.assertValid(doc)
+
+    kml = KML.kml(doc)
+
+    print(etree.tostring(format_xml_with_cdata(kml), pretty_print=True))
+
+
+if __name__ == '__main__':
+    main()
